7f53edd171f1f945d8047fb6710da09265d460eb
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-02-26 20:22:33 -0600

    crud completo de listas

--- clase1/main.py
+++ clase1/main.py
@@ -1,6 +1,6 @@
 # Listas
 
-listas = [10, 20, 30, 40, 50]
+listas = [60, 10, 20, 30, 40, 50]
 
 # AÑADIR RECURSOS A LA LISTA
 listas.append(60)
@@ -10,4 +10,19 @@ listas.insert(0, 100)
 print(listas[0])
 
 # IMPRIMIR TODOS LOS ELEMENTOS DE MI LISTA
+print(listas)
+
+# ELIMINAR ELEMENTOS
+
+listas.remove(60)
+elemento_eliminado = listas.pop(0)
+
+print(listas)
+print(elemento_eliminado)
+
+# ACTUALIZAR ELEMENTOS
+
+listas[0] = 10000
+listas[4] = 20000
+
 print(listas)
