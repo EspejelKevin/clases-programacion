5d6f2aba0ca6f0fd038fbd5a93fe7487b8cf212a
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-02-26 20:05:52 -0600

    listas seccion 1

---
+++ clase1/main.py
@@ -0,0 +1,13 @@
+# Listas
+
+listas = [10, 20, 30, 40, 50]
+
+# AÑADIR RECURSOS A LA LISTA
+listas.append(60)
+listas.insert(0, 100)
+
+# OBTENER UN ELEMENTO MEDIANTE INDICE
+print(listas[0])
+
+# IMPRIMIR TODOS LOS ELEMENTOS DE MI LISTA
+print(listas)
